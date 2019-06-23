f82ce40b1b9dfe97a25591e97281d309fe6fcbfd
Garikoitz Lerma-Usabiaga <garikoitz@gmail.com>
2019-06-23 00:32:53 -0700

    Update run_docker.sh

--- run_docker.sh
+++ run_docker.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # --entrypoint=/bin/bash \
 docker run --rm -it \
-		-v /black/localhome/glerma/soft/mrtrix3preproc/config.json:/flywheel/v0/config.json \
-        -v /black/localhome/glerma/TESTDATA/mrtrix3preproc/input:/flywheel/v0/input \
-        -v /black/localhome/glerma/TESTDATA/mrtrix3preproc/output:/flywheel/v0/output \
-        scitran-apps/mrtrix3preproc:1.0.2
+		-v /black/localhome/glerma/soft/RTP-preproc/config.json:/flywheel/v0/config.json \
+        -v /black/localhome/glerma/TESTDATA/RTP-preproc/input:/flywheel/v0/input \
+        -v /black/localhome/glerma/TESTDATA/RTP-preproc/output:/flywheel/v0/output \
+        vistalab/RTP-preproc:1.0.3
